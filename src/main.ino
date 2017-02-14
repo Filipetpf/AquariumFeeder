a0fa648ee00b6f91e50d9f979c33afec112ff5bb
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-02-14 10:44:36 -0200

    Changing code to use servo

--- src/main.ino
+++ src/main.ino
@@ -1,14 +1,32 @@
 #include <wifi.h>
 #include <update_server.h>
+#include <Servo.h>
+
+int servoPin = 14;
+int servoAngle = 0;
+
+Servo servo;
 
 void setup() {
   Serial.begin(9600);
 
-  setupWifi();
-  setupUpdateServer();
+  // setupWifi();
+  // setupUpdateServer();
+
+  servo.attach(servoPin);
+}
+
+void feedFish() {
+  servo.write(30);
+  delay(5000);
+  servo.write(110);
+  delay(10000);
 }
 
 void loop() {
-  connectWifi();
-  loopUpdateServer();
+  // connectWifi();
+  // loopUpdateServer();
+
+  feedFish();
+  delay(24*60*60*1000);
 }
