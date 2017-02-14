f3a76debd73bb34eec40509c683917235916c7c6
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-02-14 10:45:39 -0200

    Changing wifi setup to use dynamic ip address

--- lib/network/wifi.h
+++ lib/network/wifi.h
@@ -5,11 +5,6 @@ const char* network_ssid = "<SSID>";
 const char* network_password = "<PASSWORD>";
 const char* network_ssid_ap = "Rede NodeMCU";
 
-// config static IP
-IPAddress ip(192, 168, 1, 150);
-IPAddress gateway(192, 168, 1, 0);
-IPAddress subnet(255, 255, 255, 0);
-
 void connectWifi(void) {
   while(WiFi.waitForConnectResult() != WL_CONNECTED){
     WiFi.begin(network_ssid, network_password);
@@ -19,7 +14,6 @@ void connectWifi(void) {
 
 void setupWifi(void){
   WiFi.mode(WIFI_AP_STA);
-  WiFi.config(ip, gateway, subnet);
   WiFi.begin(network_ssid, network_password);
 
   connectWifi();
