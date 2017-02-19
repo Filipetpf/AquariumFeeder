f49bcb9e22a80c79bfe19ceb669e0daf8636bdd6
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-02-19 10:50:46 -0300

    Refactoring configs into file

--- lib/message/message_client.cpp
+++ lib/message/message_client.cpp
@@ -3,16 +3,16 @@
 #include <fish_feeder.cpp>
 #include <stdlib.h>
 #include <string.h>
+#include <config.h>
 
-char post_body[] = "{}";
-RestClient client = RestClient("<HOST>", 443, "<FINGERPRINT>");
+RestClient client = RestClient(GATEWAY_ADDRESS, 443, GATEWAY_SSL_FINGERPRINT);
 
 int send_message(char * message) {
   char endpoint[100];
   strcpy(endpoint, "/messages/");
   strcat(endpoint, message);
 
-  return client.post(endpoint, post_body);
+  return client.post(endpoint, "{}");
 }
 
 int check_feed_schedule() {
@@ -22,13 +22,9 @@ int check_feed_schedule() {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.parseObject(json);
 
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-  }
-
   int size = root["size"];
-
   int i;
+
   for(i = 0; i < size; i++) {
     const char* message = root["messages"][i];
     char *feed = strstr(message, "feed");
@@ -36,7 +32,7 @@ int check_feed_schedule() {
     Serial.println(message);
     if(feed != NULL && fish != NULL) {
       feed_fish();
-      send_message("The swiming creatures has been feed");
+      send_message((char*)"The swiming creatures has been feed");
     }
   }
 
--- lib/network/wifi.h
+++ lib/network/wifi.h
@@ -1,12 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-
-const char* network_ssid = "<SSID>";
-const char* network_password = "<PASSWORD>";
+#include <config.h>
 
 void connectWifi(void) {
   while(WiFi.waitForConnectResult() != WL_CONNECTED){
-    WiFi.begin(network_ssid, network_password);
+    WiFi.begin(NETWORK_SSID, NETWORK_PASSWORD);
     Serial.println("WiFi failed, retrying.");
   }
 }
