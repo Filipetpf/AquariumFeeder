ee026fd058b60da9f440a05cd98edc98dbb0c691
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-02-21 09:52:22 -0300

    Changing message endpoint

--- lib/message/message_client.cpp
+++ lib/message/message_client.cpp
@@ -11,28 +11,34 @@ int send_message(char * message, char * user_id, char * bot_id) {
   strcpy(endpoint, "/messages/");
   strcat(endpoint, message);
 
+  Serial.println(endpoint);
+
   return client.post(endpoint, "{}");
 }
 
 void check_messages(void) {
   char endpoint[100];
   strcpy(endpoint, "/messages?");
-  strcat(endpoint, "commands=feed&commands=fish");
+  strcat(endpoint, COMMANDS);
   strcat(endpoint, ALLOWED_USERS);
 
   String json;
-  client.get(endpoint, &json);
+  int status = client.get(endpoint, &json);
 
-  StaticJsonBuffer<200> jsonBuffer;
-  process_message(jsonBuffer.parseObject(json));
+  if(status == 200) {
+    StaticJsonBuffer<200> jsonBuffer;
+    process_message(jsonBuffer.parseObject(json));
+  }
 }
 
 void process_message(JsonObject& root) {
   int command_count = root["size"];
+  Serial.println(command_count);
   for(int i = 0; i < command_count; i++) {
       feed_fish();
       const char* user_id = root["messages"][i]["user_id"];
       const char* bot_id = root["messages"][i]["bot_id"];
+
       send_message((char*) "The swiming creatures has been feed", (char*) user_id, (char*) bot_id);
   }
 }
