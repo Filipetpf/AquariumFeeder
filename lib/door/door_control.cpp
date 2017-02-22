c0c9863950eaeca8e1a3c2ff07a0f7ca17931328
Dawson Freitas Israel <dawsonfi@gmail.com>
2017-02-22 13:40:14 -0300

    Change door controll pin activation

--- lib/door/door_control.cpp
+++ lib/door/door_control.cpp
@@ -3,11 +3,11 @@
 
 void setup_door(void) {
   pinMode(DOOR_PIN, OUTPUT);
-  digitalWrite(DOOR_PIN, LOW);
+  digitalWrite(DOOR_PIN, HIGH);
 }
 
 void open_door(void) {
   digitalWrite(DOOR_PIN, LOW);
-  delay(1000);  
+  delay(1000);
   digitalWrite(DOOR_PIN, HIGH);
 }
